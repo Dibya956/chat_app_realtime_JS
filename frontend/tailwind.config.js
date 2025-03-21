5d3ce3ba50dd49aa183b3d73d1c72cd9a9b9df41
Dibyakalyan Ratha <123139327+Dibya956@users.noreply.github.com>
2025-03-21 22:55:41 +0530

    Update tailwind.config.js
    Converted to ES6

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+import daisyui from "daisyui";
+
+const config = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [daisyui],
 };
+
+export default config;
